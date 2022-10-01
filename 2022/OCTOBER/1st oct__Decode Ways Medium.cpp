695bc0dc0892bffbfbadb45ccec378e213cf32e1
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-01 03:18:32 -0700

    Update 1st oct__Decode Ways Medium.cpp

--- 2022/OCTOBER/1st oct__Decode Ways Medium.cpp
+++ 2022/OCTOBER/1st oct__Decode Ways Medium.cpp
@@ -8,3 +8,13 @@ at first mere se ye sawal ni hua
 eg for 226 ...we can generate (2,2,6) ,(22,6) , (6,22)
 2.for a given value how to get its corresponding key in a map
 */
+
+
+
+/*OSERVATIONS
+kind of backtracking question bol sakte ho
+1.recursion lag raha yahan pe .....  now the question is ye kaise pata chala....
+A.coz for every element in string we have "choice" ki usko len ki ni.....so jab bhi haamare pass choice ho then think about recursion
+B.question me bola hai return the number of ways to decode it.....so jab bhi no of ways ki bat ho think about recursion 👀
+*/
+
