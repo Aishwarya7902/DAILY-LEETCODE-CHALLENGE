8c45d869de97f21433b9713bbc6a8270bc8569a0
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-01 06:06:12 -0700

    Update 1st oct__Decode Ways Medium.cpp

--- 2022/OCTOBER/1st oct__Decode Ways Medium.cpp
+++ 2022/OCTOBER/1st oct__Decode Ways Medium.cpp
@@ -18,3 +18,28 @@ A.coz for every element in string we have "choice" ki usko len ki ni.....so jab 
 B.question me bola hai return the number of ways to decode it.....so jab bhi no of ways ki bat ho think about recursion 👀
 */
 
+video: https://www.youtube.com/watch?v=UhxJq2eZFes
+/*
+recursion gives tle hence memoization
+*/
+
+class Solution {
+private:
+  int find(int i,string s,vector<int>&dp){
+    if(s[i]=='0')return 0;
+    if(i==s.size())return 1;
+    if(dp[i]!=-1)return dp[i];
+    int one=find(i+1,s,dp);
+    int two=0;
+    if(i<s.size()-1 && (s[i]=='1' || s[i]=='2' && s[i+1]<='6')){
+      two+=find(i+2,s,dp);
+    }
+    return dp[i]=one+two;
+  }
+public:
+    int numDecodings(string s) {
+      vector<int>dp(s.size()+1,-1);
+      return find(0,s,dp);  
+    }
+};
+
