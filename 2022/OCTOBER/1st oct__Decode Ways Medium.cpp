5d8f681aea03a4c9a3aec682890000b5debca191
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-01 02:54:54 -0700

    Create 1st oct__Decode Ways Medium.cpp

---
+++ 2022/OCTOBER/1st oct__Decode Ways Medium.cpp
@@ -0,0 +1,10 @@
+PROBLREM: https://leetcode.com/problems/decode-ways/
+/*
+FEEDBACK (KYU NI HUA)
+at first mere se ye sawal ni hua
+
+
+1.given a string of numbers....mujhe samjh ni aa raha ki uske corresponding possible number combination kaise generate karen
+eg for 226 ...we can generate (2,2,6) ,(22,6) , (6,22)
+2.for a given value how to get its corresponding key in a map
+*/
