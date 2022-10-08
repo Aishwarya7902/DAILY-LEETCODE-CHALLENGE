44c3f2a6015171298d2dee9c6bdd601e236b460c
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-08 07:47:51 -0700

    Update 3 sum.cpp

--- 2022/OCTOBER/3 sum.cpp
+++ 2022/OCTOBER/3 sum.cpp
@@ -39,3 +39,41 @@ public:
       return ans;
     }
 };
+
+/*
+optimised
+for explanation see striver bhaiya's video:https://www.youtube.com/watch?v=onLoX6Nhvmg&t=352s
+tc: o(n*n) //see video for clarity
+sc:O(1)
+*/
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+       int n=nums.size();
+       vector<vector<int>>ans;
+      for(int i=0;i<n-2;i++){
+        if(i==0 || (i>0 && nums[i]!=nums[i-1] )){
+          int lo=i+1,hi=n-1,sum=0-nums[i];
+          while(lo<hi){
+            if(nums[lo]+nums[hi]==sum){
+             vector<int>temp;
+             temp.push_back(nums[i]);
+             temp.push_back(nums[lo]);
+             temp.push_back(nums[hi]);
+              ans.push_back(temp);
+              while(lo<hi && nums[lo]==nums[lo+1])lo++;
+              while(lo<hi && nums[hi]==nums[hi-1])hi--;
+              lo++;
+              hi--;
+              
+            }
+            else if(nums[lo]+nums[hi]<sum)lo++;
+            else hi--;
+          
+        }
+      }
+      }
+        return ans;
+    }
+};
