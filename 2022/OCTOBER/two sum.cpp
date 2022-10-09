735df2f152d4056b553f92b7f57ce9f8f37349b7
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-09 01:59:11 -0700

    Update two sum.cpp

--- 2022/OCTOBER/two sum.cpp
+++ 2022/OCTOBER/two sum.cpp
@@ -27,3 +27,28 @@ public:
       return ans;
     }
 };
+
+/*
+optimised
+using hashing
+best explaination(striver bhaiya):https://www.youtube.com/watch?v=dRUpbt8vHpo
+tc: O(n) //for loop +O(1)//for insertion and searching in an unordered map
+sc:O(n) //for storing in map
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+          if(mp.find(target-nums[i])!=mp.end()){
+            ans.push_back(mp[target-nums[i]]);
+            ans.push_back(i);
+            return ans;
+          }
+          mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
