aa1fad7db5ba92e43305c44fbc2453f04ea75c33
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-12 11:12:17 -0700

    Create 12th oct__Largest Perimeter Triangle Easy.cpp

---
+++ 2022/OCTOBER/12th oct__Largest Perimeter Triangle Easy.cpp
@@ -0,0 +1,20 @@
+PROBLEM:https://leetcode.com/problems/largest-perimeter-triangle/
+
+/*
+tc:O(n)
+sc:O(1)
+pseudo code: sum of  two smaller sides should be greater than the largest side
+*/
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=nums.size()-3;i>=0;i--){
+          if(nums[i]+nums[i+1]>nums[i+2]){
+            return nums[i]+nums[i+1]+nums[i+2];
+          }
+        }
+      return 0;
+    }
+};
