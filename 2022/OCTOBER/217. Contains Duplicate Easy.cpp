e43c818a9a3fdea92d84773b528159537c7021c0
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-10-21 08:10:11 -0700

    Update 217. Contains Duplicate Easy.cpp

--- 2022/OCTOBER/217. Contains Duplicate Easy.cpp
+++ 2022/OCTOBER/217. Contains Duplicate Easy.cpp
@@ -62,3 +62,22 @@ public:
       return nums.size()> set<int>(nums.begin(),nums.end()).size();  
     }
 };
+
+/*method 4
+tc:
+sc:
+pseudo code:
+1.sort the array
+2.traverse and if any adjacent elements are equla ...return true;
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+      for(int i=1;i<nums.size();i++){
+        if(nums[i]==nums[i-1])return true;
+      }
+      return false;
+    }
+};
