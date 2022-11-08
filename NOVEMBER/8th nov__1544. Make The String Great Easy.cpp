429b4b00acf98ac9df6d313da2a053d0a9182070
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-11-08 00:35:55 -0800

    Create 8th nov__1544. Make The String Great Easy.cpp

---
+++ NOVEMBER/8th nov__1544. Make The String Great Easy.cpp
@@ -0,0 +1,34 @@
+PROBLEM: https://leetcode.com/problems/make-the-string-great/
+VIDEO:   https://www.youtube.com/watch?v=0Yfkz6ZBWE8
+ /*
+ METHOD 1__USING STACK
+ TC:
+ SC:
+ */
+ 
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string makeGood(string s) {
+       stack<char>st;
+       for(int i=0;i<s.size();i++){
+         if(st.empty())
+         {
+           st.push(s[i]);
+           continue;
+         }
+         if(abs(st.top()-s[i])==32){
+           st.pop();
+         }
+         else{
+           st.push(s[i]);
+         }
+       }
+      string ans="";
+      while(!st.empty()){
+        ans=st.top()+ans;
+        st.pop();
+      }
+      return ans;
+    }
+};
