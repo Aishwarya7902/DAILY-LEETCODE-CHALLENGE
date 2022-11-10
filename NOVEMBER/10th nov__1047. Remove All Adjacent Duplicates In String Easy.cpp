2bda0bb3e3ddc58a56a53f3831c46238608c7ff3
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-11-09 22:10:57 -0800

    Update 10th nov__1047. Remove All Adjacent Duplicates In String Easy.cpp

--- NOVEMBER/10th nov__1047. Remove All Adjacent Duplicates In String Easy.cpp
+++ NOVEMBER/10th nov__1047. Remove All Adjacent Duplicates In String Easy.cpp
@@ -1,8 +1,9 @@
 PROBLEM: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
-VIDEO:
+VIDEO:https://www.youtube.com/watch?v=52TwSRU5NJM
 /*
+ONE PASS
 TC:O(N)
-SC:O(1)
+SC:O(N)
 QUESTION IS QUITE SIMILAR TO YESTERDAY'S PROBLEM
 */
 
@@ -24,5 +25,35 @@ public:
       }
       
       return st;
+    }
+};
+
+/*METHOD 2 (TWO PASS SOLUTION)
+(USING STACK)
+TC:O(N) 
+SC:O(M+N)coz we are using two spaces one for stack and one for string
+*/
+
+class Solution {
+public:
+  //tc:o(n)
+  //sc:o(n)
+    string removeDuplicates(string s) {
+     stack<char>st;
+      for(int i=0;i<s.size();i++){
+        
+         if(!st.empty() && st.top()==s[i]){
+          st.pop();
+        }
+        else{
+          st.push(s[i]);
+        }
+      }
+      string ans="";
+      while(!st.empty()){
+        ans=st.top()+ans;
+        st.pop();
+      }
+      return ans;
     }
 };
