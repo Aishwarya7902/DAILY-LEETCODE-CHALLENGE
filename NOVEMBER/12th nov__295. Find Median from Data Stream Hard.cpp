11a16343c4d3aea2095af9bb3bf679eb708585dd
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-11-12 06:05:01 -0800

    Update 12th nov__295. Find Median from Data Stream Hard.cpp

--- NOVEMBER/12th nov__295. Find Median from Data Stream Hard.cpp
+++ NOVEMBER/12th nov__295. Find Median from Data Stream Hard.cpp
@@ -37,3 +37,44 @@ public:
     }
 };
 
+/*
+method 2
+tc: O(1) //coz top element nikal rahe + log(n) for insertion in priority queue 
+sc:
+*/
+
+class MedianFinder {
+public:
+  priority_queue<int>mx_heap;
+  priority_queue<int,vector<int>,greater<int>>min_heap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+     if(mx_heap.size()>0 && num>mx_heap.top()){
+       min_heap.push(num);
+     } 
+      else{
+        mx_heap.push(num);
+      }
+      if(mx_heap.size()>min_heap.size()+1){
+        min_heap.push(mx_heap.top());
+        mx_heap.pop();
+      }
+      if(min_heap.size()>mx_heap.size()+1){
+        mx_heap.push(min_heap.top());
+        min_heap.pop();
+      }
+    }
+    
+    double findMedian() {
+        if(min_heap.size()==mx_heap.size()){
+          return (min_heap.top()+mx_heap.top())/2.0;
+        }
+      else if(mx_heap.size()>min_heap.size()){
+        return mx_heap.top();
+      }
+      return min_heap.top();
+    }
+};
