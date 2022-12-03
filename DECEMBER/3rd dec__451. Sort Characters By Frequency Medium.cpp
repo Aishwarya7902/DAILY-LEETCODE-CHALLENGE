6fb280ad35759ef4633293e65908e4d0dc077d37
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-02 23:41:21 -0800

    Update 3rd dec__451. Sort Characters By Frequency Medium.cpp

--- DECEMBER/3rd dec__451. Sort Characters By Frequency Medium.cpp
+++ DECEMBER/3rd dec__451. Sort Characters By Frequency Medium.cpp
@@ -44,3 +44,60 @@ public:
       return ans;
     }
 };
+
+
+/* USING HEAP
+INTUTION :
+
+1.The idea is to use max heap with nodes containing a pair {frequency, character}
+2.Since the first element of the pair is frequency, we don't need to pass a custom comparator
+3. Max heap will be constructed on the basis of frequency of characters
+4. the root will contain the character with maximum frequency.
+
+TC:O(log(n)) For Construction and extraction from heap
++
+O(n) For storing the frequency in hashmap.
+SC: o(n)
+PSEUDO CODE:
+1.take a map and store frequency of each character
+2.TAVERSE MAP AND STORE IT'S FREQ,CHAR IN THE MAXHEAP
+ Since the first element of the pair is frequency, we don't need to pass a custom comparator
+3.take each node of heap and  make a string ( count, character )
+create a string containing characters repeated count times ...and then pop
+4.return final ans 
+*/
+
+#include<bits/stdc++.h>
+class Solution {  
+public:
+  
+    string frequencySort(string s) {
+     unordered_map<char,int>freq;
+      priority_queue<pair<int,char>>maxheap;
+      
+    string ans;
+      //STORE FREQUENCY OF EACH CHARACTER IN A MAP
+     for(auto x:s){
+       freq[x]++;
+     }
+      //TAVERSE MAP AND STORE IT'S FREQ,CHAR IN THE MAXHEAP
+      //Since the first element of the pair is frequency, we don't need to pass a custom comparator
+      for(auto it:freq){
+        maxheap.push({it.second,it.first});  
+      }
+      
+      /*take each node of heap and  make a string ( count, character )
+
+create a string containing characters repeated count times ...and then pop
+*/
+      
+      while(maxheap.size()){
+        ans+=string(maxheap.top().first,maxheap.top().second);  //LEARNING🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+        maxheap.pop();
+      }
+      
+      
+     //return final ans 
+      return ans;
+    }
+};
