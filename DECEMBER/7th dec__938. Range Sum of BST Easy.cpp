67415ff2e5d25a28f6f3feea9c54cf3f2ffe90fc
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-06 22:31:04 -0800

    Update 7th dec__938. Range Sum of BST Easy.cpp

--- DECEMBER/7th dec__938. Range Sum of BST Easy.cpp
+++ DECEMBER/7th dec__938. Range Sum of BST Easy.cpp
@@ -1,11 +1,11 @@
 PROBLEM:https://leetcode.com/problems/range-sum-of-bst/
-VIDEO:
+VIDEO(AYUSHI SHERMA):https://www.youtube.com/watch?v=wIloFJzSOfE
 
 /*
 METHOD 1
 SIMPLE PREORDER TRAVERSAL
 TC:O(N)
-SC:O(N) //auxiliary space for recursion
+SC:O(h) //auxiliary space for recursion
 
 */
 
@@ -19,6 +19,40 @@ void helpSum(TreeNode* root, int low, int high,int &sum){
     }
     helpSum(root->left,low,high,sum);
     helpSum(root->right,low,high,sum);
+
+}
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum=0;
+        helpSum(root,low,high,sum);
+        return sum;
+    }
+};
+
+/*
+METHOD 2
+making use of the fact that it is a BST
+TC:O(N)
+SC:O(h) 
+*/
+
+class Solution {
+private:
+void helpSum(TreeNode* root, int low, int high,int &sum){
+    if(!root)
+    return ;
+    if(root->val>=low && root->val<=high){
+        sum+=root->val;
+    
+    helpSum(root->left,low,high,sum);
+    helpSum(root->right,low,high,sum);
+    }
+    else if (root->val<low){
+       helpSum(root->right,low,high,sum); 
+    }
+    else{
+        helpSum(root->left,low,high,sum);
+    }
 
 }
 public:
