e32a521063fda182bca014d3bb3ca00be1635d55
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-10 10:19:28 -0800

    Create 10th dec__1339. Maximum Product of Splitted Binary Tree.cpp

---
+++ DECEMBER/10th dec__1339. Maximum Product of Splitted Binary Tree.cpp
@@ -0,0 +1,46 @@
+PROBLEM: https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/description/
+
+video (BEST VIDEO EVER) :https://www.youtube.com/watch?v=ITySQXe9Ybg
+
+/*
+METHOD 1:
+TC:
+SC:
+*/
+
+class Solution {
+private:
+long SUM=0;
+long maxP=0;
+int totalSum(TreeNode* root){
+    if(root==NULL)
+     return 0;
+
+    int leftSubtreeSum=totalSum(root->left);
+    int rightSubtreeSum=totalSum(root->right);
+    int treeSum=root->val+leftSubtreeSum+rightSubtreeSum;
+    return treeSum;
+}
+int find(TreeNode* root){
+    if(root==NULL)
+     return 0;
+    int leftSum=find(root->left);
+    int rightSum=find(root->right);
+    long subtreeSum=root->val+leftSum+rightSum; //s1
+    long remainingSum=SUM-subtreeSum;
+    maxP=max(maxP,subtreeSum*remainingSum);
+    return subtreeSum;
+
+
+}
+public:
+    
+    int maxProduct(TreeNode* root) {
+       if(root==NULL)
+        return 0;
+       maxP=0;
+       SUM=totalSum(root);
+       find(root);
+       return maxP%(1000000007); 
+    }
+};
