4867487caea95bf1ba0d675185f9d75ff70b1c76
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-11 23:07:10 -0800

    Create 12th dec__70. Climbing Stairs Easy.cpp

---
+++ DECEMBER/12th dec__70. Climbing Stairs Easy.cpp
@@ -0,0 +1,23 @@
+PROBLEM:https://leetcode.com/problems/climbing-stairs/description/
+VIDEO (BEST ):
+
+/*
+SELF  (USING DP)
+TC:
+SC:
+
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+         vector<int>dp(n+1);
+         dp[0]=1;
+         dp[1]=1;
+         for(int i=2;i<=n;i++){
+             dp[i]=dp[i-1]+dp[i-2];
+         }
+         return dp[n];
+    }
+};
