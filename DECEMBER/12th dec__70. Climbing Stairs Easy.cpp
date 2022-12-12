730e60f0b7441310d59e2dbc0a171253f703b353
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-11 23:58:43 -0800

    Update 12th dec__70. Climbing Stairs Easy.cpp

--- DECEMBER/12th dec__70. Climbing Stairs Easy.cpp
+++ DECEMBER/12th dec__70. Climbing Stairs Easy.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/climbing-stairs/description/
-VIDEO (BEST ):
+VIDEO (BEST ): https://www.youtube.com/watch?v=6OjGE04Kx_M
 
 /*
 SELF  (USING DP) -BOTTOM UP
@@ -14,11 +14,13 @@ SC:
 class Solution {
 public:
     int climbStairs(int n) {
-        
+         if(n==1 || n==2)
+         return n;
          vector<int>dp(n+1);
-         dp[0]=1;
+         dp[0]=0;
          dp[1]=1;
-         for(int i=2;i<=n;i++){
+         dp[2]=2;
+         for(int i=3;i<=n;i++){
              dp[i]=dp[i-1]+dp[i-2];
          }
          return dp[n];
