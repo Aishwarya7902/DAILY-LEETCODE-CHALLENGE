0afe227d8c50bc352232cdad2ddf14f4d12918f2
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-13 00:19:51 -0800

    Update 13th dec__931. Minimum Falling Path Sum.cpp

--- DECEMBER/13th dec__931. Minimum Falling Path Sum.cpp
+++ DECEMBER/13th dec__931. Minimum Falling Path Sum.cpp
@@ -30,3 +30,35 @@ public:
       return mini; 
     }
 };
+
+/*
+METHOD 2:
+overlapping sub__problems
+TC: O(N*M)
+SC: O(N*M)->for dp matrix + O(n)->for recursion stack space
+*/
+
+class Solution {
+private:
+int helper(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>&dp){
+    if(j<0 || j>=matrix[0].size()) return 1e8;
+    if(i==0)return matrix[0][j];//base case
+    if(dp[i][j]!=-1)return dp[i][j];
+    int u=matrix[i][j]+helper(i-1,j,matrix,dp);
+    int ld=matrix[i][j]+helper(i-1,j-1,matrix,dp);
+    int rd=matrix[i][j]+helper(i-1,j+1,matrix,dp);
+    return dp[i][j]=min({u,ld,rd});
+}
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+      int n=matrix.size();
+      int m=matrix[0].size();
+      int mini=1e8;
+      vector<vector<int>>dp(n,vector<int>(m,-1));
+      for(int j=0;j<m;j++){
+          mini=min(mini,helper(n-1,j,matrix,dp));
+      } 
+      return mini; 
+    }
+};
+
