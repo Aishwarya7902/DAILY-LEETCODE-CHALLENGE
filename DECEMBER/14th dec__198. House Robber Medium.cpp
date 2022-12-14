07ad4ec3cc7c48b3ac1eaf440f3603a3609c705f
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-14 04:37:23 -0800

    Update 14th dec__198. House Robber Medium.cpp

--- DECEMBER/14th dec__198. House Robber Medium.cpp
+++ DECEMBER/14th dec__198. House Robber Medium.cpp
@@ -72,7 +72,33 @@ public:
         return dp[n-1];
     }
 };
-                                 
+
+/*
+SPACE OPTIMISATION
+TC:O(N)
+SC:O(1)
+*/
+
+class Solution {
+
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1)
+             take+=prev2;
+            int notTake=0+prev;
+           int curr=max(take,notTake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
                                  
                                 
                                  
