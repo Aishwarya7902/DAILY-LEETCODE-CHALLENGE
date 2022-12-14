5d9cc726c682e67e62f0942502d4faefe4271e2e
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-14 03:42:05 -0800

    Update 14th dec__198. House Robber Medium.cpp

--- DECEMBER/14th dec__198. House Robber Medium.cpp
+++ DECEMBER/14th dec__198. House Robber Medium.cpp
@@ -13,7 +13,7 @@ int helper(vector<int>& nums,int n){
     if(n<=0)
      return 0;
      
-    int option1=nums[n-1]+helper(nums,n-2;
+    int option1=nums[n-1]+helper(nums,n-2);
     int option2=helper(nums,n-1);
     return max(option1,option2);
 
@@ -25,3 +25,31 @@ public:
        return helper(nums,n,dp); 
     }
 };
+                                 
+/*
+MEMOIZATION
+TC: O(N)
+SC: O(N) //auxiliary stack space +O(N) //for dp vector
+*/
+class Solution {
+int helper(vector<int>& nums,int n,vector<int>&dp){
+    if(n<=0)
+     return 0;
+     if(dp[n]!=-1)return dp[n];
+    int option1=nums[n-1]+helper(nums,n-2,dp);
+    int option2=helper(nums,n-1,dp);
+    return dp[n]=max(option1,option2);
+
+}
+public:
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+       vector<int>dp(n+1,-1);
+       return helper(nums,n,dp); 
+    }
+};
+                                 
+                                 
+                                 
+                                
+                                 
