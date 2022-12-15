0eec0e0be8906b57fb94518c64b7035d7671ec69
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-15 02:17:05 -0800

    Update 15th dec__1143. Longest Common Subsequence Medium.cpp

--- DECEMBER/15th dec__1143. Longest Common Subsequence Medium.cpp
+++ DECEMBER/15th dec__1143. Longest Common Subsequence Medium.cpp
@@ -30,7 +30,7 @@ public:
 /*
 MEMOIZATION
 TC:O(n*m)
-SC:O(n*m)
+SC:O(n*m)// for dp vector + O(n+m) //for auxiliary stack space
 */
 class Solution {
 private:
@@ -51,5 +51,61 @@ public:
       int m=text2.size();//size of string2
       vector<vector<int>>dp(n,vector<int>(m,-1));
       return helpLcs(text1,n-1,text2,m-1,dp);  
+    }
+};
+
+/*
+METHOD 3 
+INDEX SHIFTING (WATCH STRIVER BHAIYA'S VIDEO: https://youtu.be/NPZn9jBrX8U?t=2292
+*/
+
+class Solution {
+private:
+int helpLcs(string text1,int indx1,string text2,int indx2,vector<vector<int>>&dp){
+    if(indx1==0 || indx2==0)return 0; //negative index implies end of string
+    //if both indexes matches
+    if(dp[indx1][indx2]!=-1)return dp[indx1][indx2];
+    if(text1[indx1-1]==text2[indx2-1]){
+        return dp[indx1][indx2]=1+helpLcs(text1,indx1-1,text2,indx2-1,dp);
+    }
+    //no match....then try out both possibilities
+    return dp[indx1][indx2]=max(helpLcs(text1,indx1,text2,indx2-1,dp),helpLcs(text1,indx1-1,text2,indx2,dp));
+
+}
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+      int n=text1.size();//size of string1
+      int m=text2.size();//size of string2
+      vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+      return helpLcs(text1,n,text2,m,dp);  
+    }
+};
+
+/*
+TABULATION:
+TC:O(n*m)
+SC:O(n*m)// for dp vector
+*/class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+      int n=text1.size();//size of string1
+      int m=text2.size();//size of string2
+      vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+      for(int j=0;j<=m;j++)dp[0][j]=0;
+      for(int i=0;i<=n;i++)dp[i][0]=0;
+
+
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+              if(text1[i-1]==text2[j-1]){
+        dp[i][j]=1+dp[i-1][j-1];
+    }
+    //no match....then try out both possibilities
+   else
+    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+
+          }
+      }
+      return dp[n][m];
     }
 };
