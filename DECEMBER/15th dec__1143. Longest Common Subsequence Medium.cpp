710b58c2fd4865589c72a1028861f6f6a503e261
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-15 02:26:53 -0800

    Update 15th dec__1143. Longest Common Subsequence Medium.cpp

--- DECEMBER/15th dec__1143. Longest Common Subsequence Medium.cpp
+++ DECEMBER/15th dec__1143. Longest Common Subsequence Medium.cpp
@@ -109,3 +109,35 @@ public:
       return dp[n][m];
     }
 };
+
+
+/*METHOD 5
+SPACE OPTIMIZATION
+TC:O(n*m)
+SC:O(m)
+*/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+      int n=text1.size();//size of string1
+      int m=text2.size();//size of string2
+      vector<int> prev(m+1,0),curr(m+1,0);
+      for(int j=0;j<=m;j++)prev[j]=0;
+      
+
+
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+              if(text1[i-1]==text2[j-1]){
+        curr[j]=1+prev[j-1];
+    }
+    //no match....then try out both possibilities
+   else
+    curr[j]=max(curr[j-1],prev[j]);
+
+          }
+          prev=curr;
+      }
+      return curr[m];
+    }
+};
