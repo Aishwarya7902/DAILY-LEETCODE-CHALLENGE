475df94020840d9b6f29c3a5fa5cc867c1eaae37
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-15 01:33:03 -0800

    Update 15th dec__1143. Longest Common Subsequence Medium.cpp

--- DECEMBER/15th dec__1143. Longest Common Subsequence Medium.cpp
+++ DECEMBER/15th dec__1143. Longest Common Subsequence Medium.cpp
@@ -26,3 +26,30 @@ public:
       return helpLcs(text1,n-1,text2,m-1);  
     }
 };
+
+/*
+MEMOIZATION
+TC:O(n*m)
+SC:O(n*m)
+*/
+class Solution {
+private:
+int helpLcs(string text1,int indx1,string text2,int indx2,vector<vector<int>>&dp){
+    if(indx1<0 || indx2<0)return 0; //negative index implies end of string
+    //if both indexes matches
+    if(dp[indx1][indx2]!=-1)return dp[indx1][indx2];
+    if(text1[indx1]==text2[indx2]){
+        return dp[indx1][indx2]=1+helpLcs(text1,indx1-1,text2,indx2-1,dp);
+    }
+    //no match....then try out both possibilities
+    return dp[indx1][indx2]=max(helpLcs(text1,indx1,text2,indx2-1,dp),helpLcs(text1,indx1-1,text2,indx2,dp));
+
+}
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+      int n=text1.size();//size of string1
+      int m=text2.size();//size of string2
+      vector<vector<int>>dp(n,vector<int>(m,-1));
+      return helpLcs(text1,n-1,text2,m-1,dp);  
+    }
+};
