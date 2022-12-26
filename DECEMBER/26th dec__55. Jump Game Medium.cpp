daae3286e77536e13fd4a756913bc7d6f247fd4d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-26 00:20:08 -0800

    Update 26th dec__55. Jump Game Medium.cpp

--- DECEMBER/26th dec__55. Jump Game Medium.cpp
+++ DECEMBER/26th dec__55. Jump Game Medium.cpp
@@ -27,3 +27,32 @@ public:
         return solve(nums,n,0);
     }
 };
+
+/*
+memoization:
+tc: 
+sc:
+*/
+
+
+class Solution {
+private:
+int dp[10001];
+bool solve(vector<int>& nums,int n,int indx){
+    if(indx==n-1)return true; 
+    if(indx>n-1)return false; 
+    if(dp[indx]!=-1)return dp[indx];
+    for(int i=1;i<=nums[indx];i++){
+        if(solve(nums,n,indx+i))return dp[indx]=true;
+    }
+    return dp[indx]=false;
+}
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size();
+        //call our solve function
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,n,0);
+    }
+};
