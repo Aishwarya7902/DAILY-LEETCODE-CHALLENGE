a9948155efeb7103bfb16fe13c7fd665bfd746b9
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-27 02:08:33 -0800

    Update 27th dec__Maximum Bags With Full Capacity of Rocks Medium.cpp

--- DECEMBER/27th dec__Maximum Bags With Full Capacity of Rocks Medium.cpp
+++ DECEMBER/27th dec__Maximum Bags With Full Capacity of Rocks Medium.cpp
@@ -33,3 +33,33 @@ public:
        return cnt;
     }
 };
+
+/*little better code
+*/
+
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n=capacity.size();
+       vector<int>require(n,0);
+       //travel to get the required rocks to completely fill
+       //i.e to reuired=capacity-currently present in bag
+       for(int i=0;i<n;i++){
+       require[i]=capacity[i]-rocks[i];
+       } 
+       //sort because we will try to maximize bags with full capacity by using additionalRocks
+       sort(require.begin(),require.end());
+       int cnt=0;
+       for(int i=0;i<n;i++){
+           if(additionalRocks>=require[i]){
+               additionalRocks-=require[i];
+               cnt++;
+           }
+           else{
+               break;
+           }
+       }
+    
+       return cnt;
+    }
+};
