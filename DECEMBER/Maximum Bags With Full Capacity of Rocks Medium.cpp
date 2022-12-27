0881e05cb06937b1e509e6096f2c50c16c8fbd57
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-27 01:44:18 -0800

    Create Maximum Bags With Full Capacity of Rocks Medium.cpp

---
+++ DECEMBER/Maximum Bags With Full Capacity of Rocks Medium.cpp
@@ -0,0 +1,35 @@
+PROBLEM:
+VIDEO:
+
+/*
+BRUTE FORCE
+TC:O(N)+O(NlogN)
+SC:
+*/
+
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n=capacity.size();
+       vector<int>require(n,0);
+       //travel to get the required rocks to completely fill
+       //i.e to reuired=capacity-currently present in bag
+       for(int i=0;i<n;i++){
+       require[i]=capacity[i]-rocks[i];
+       } 
+       //sort because we will try to maximize bags with full capacity by using additionalRocks
+       sort(require.begin(),require.end());
+       for(int i=0;i<n;i++){
+           if(additionalRocks>=require[i]){
+               additionalRocks-=require[i];
+               require[i]=0;
+           }
+       }
+       //see how many bags are completely filled
+       int cnt=0;
+       for(auto x:require){
+           if(x==0)cnt++;
+       }
+       return cnt;
+    }
+};
