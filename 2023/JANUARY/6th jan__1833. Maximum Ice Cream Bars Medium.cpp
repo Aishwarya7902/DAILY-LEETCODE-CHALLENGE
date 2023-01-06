55a583e953b888e7b453d78dd0c478950761a91d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-05 22:43:15 -0800

    Create 6th jan__1833. Maximum Ice Cream Bars Medium.cpp

---
+++ 2023/JANUARY/6th jan__1833. Maximum Ice Cream Bars Medium.cpp
@@ -0,0 +1,25 @@
+PROBLEM:https://leetcode.com/problems/maximum-ice-cream-bars/description/
+VIDEO:
+
+/*
+DONE BY MYSELF
+*/
+
+class Solution {
+public:
+//tc:o(nlogn)+o(n)
+//sc(1) we are not using any extra space
+    int maxIceCream(vector<int>& costs, int coins) {
+        //sort the cost array
+        sort(costs.begin(),costs.end());//tc:o(nlogn)
+        int maxIceBars=0;
+        for(int i=0;i<costs.size();i++){//tc:o(n)
+            if(costs[i]>coins)return maxIceBars;
+            else{
+                maxIceBars++;
+                coins-=costs[i];
+            }
+        }
+        return maxIceBars;
+    }
+};
