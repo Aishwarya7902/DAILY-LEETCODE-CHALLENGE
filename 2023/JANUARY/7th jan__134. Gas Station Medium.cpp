8d77048e89578e6e8297bf465f8acae7c40754c6
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-07 02:03:43 -0800

    Update 7th jan__134. Gas Station Medium.cpp

--- 2023/JANUARY/7th jan__134. Gas Station Medium.cpp
+++ 2023/JANUARY/7th jan__134. Gas Station Medium.cpp
@@ -29,3 +29,31 @@ public:
         return -1;
     }
 };
+
+
+/*
+optimisation 
+using greedy
+tc:o(n)
+sc:o(1)
+*/
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int  n=gas.size();
+        int totalKamai=accumulate(begin(gas),end(gas),0);
+        int totalKharcha=accumulate(begin(cost),end(cost),0);
+        if(totalKamai<totalKharcha)return -1;
+        int total=0;
+        int result_index=0;
+        for(int i=0;i<n;i++){
+            total+=gas[i]-cost[i];
+            if(total<0){
+                result_index=i+1;
+                total=0;
+            }
+        }
+        return result_index;
+    }
+};
