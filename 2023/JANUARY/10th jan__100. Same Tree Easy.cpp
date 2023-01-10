4885176ed55b8464455b69ad8dba6a881676baba
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-10 00:38:33 -0800

    Update 10th jan__100. Same Tree Easy.cpp

--- 2023/JANUARY/10th jan__100. Same Tree Easy.cpp
+++ 2023/JANUARY/10th jan__100. Same Tree Easy.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/same-tree/description/
-VIDEO (MAZHAR BHAIYA) : 
+VIDEO (MAZHAR BHAIYA) : https://www.youtube.com/watch?v=YtoibyDlzk0
 
 /*
 tc:o(n)
@@ -21,6 +21,51 @@ bool solve(TreeNode* p, TreeNode* q) {
         if(p->val !=q->val)return false;
         if(!solve(p->left,q->left))return false;
         if(!solve(p->right,q->right))return false;
+        return true;
+
+    }
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return solve(p,q);
+    }
+};
+
+
+/*METHOD 2
+ITERATIVE :USING BFS
+*/
+
+class Solution {
+private:
+bool solve(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)return true;
+        if(p==NULL || q==NULL )return false;
+        queue<TreeNode*>q1;
+        queue<TreeNode*>q2;
+        q1.push(p);
+        q2.push(q);
+        while(!q1.empty() && !q2.empty()){
+            TreeNode* node1=q1.front();
+            q1.pop();
+            TreeNode* node2=q2.front();
+            q2.pop();
+            if(node1->val != node2->val)return false;
+            if(node1->left!=NULL && node2->left!=NULL){
+                q1.push(node1->left);
+                q2.push(node2->left);
+
+            }
+            else if(node1->left!=NULL || node2->left!=NULL)
+             return false;
+             if(node1->right!=NULL && node2->right!=NULL){
+                q1.push(node1->right);
+                q2.push(node2->right);
+
+            }
+            else if(node1->right!=NULL || node2->right!=NULL)
+             return false;
+        }
+        
         return true;
 
     }
