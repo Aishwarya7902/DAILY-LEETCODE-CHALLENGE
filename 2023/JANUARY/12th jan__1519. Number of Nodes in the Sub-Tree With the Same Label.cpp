db4efb33dd388e2cf44d50b5903b45b8ce341816
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-12 03:05:16 -0800

    Create 12th jan__1519. Number of Nodes in the Sub-Tree With the Same Label.cpp

---
+++ 2023/JANUARY/12th jan__1519. Number of Nodes in the Sub-Tree With the Same Label.cpp
@@ -0,0 +1,55 @@
+PROBLEM:https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/description/
+VIDEO(MAZHAR BHAIYA):
+
+/*
+NAIVE SOLUTION :giving tle
+PSEUDO CODE:
+1.for every node call bfs
+2.in bfs if its label is same as parent label cnt++
+3.ans[i]=cnt
+4.repeat the above steps for every node
+
+tc:o(n^2) //coz for every node we are traversing the whole tree
+sc:
+*/
+
+class Solution {
+private:
+int bfs(unordered_map<int,vector<int>>&adj,int node,char label,string labels){
+    int cntSame=0;
+    queue<int>q;
+    q.push(node);
+    while(!q.empty()){
+       int curr=q.front();
+       q.pop();
+       if(labels[node]==label)cntSame++;
+       for(auto &child:adj[node]){
+           if(child==node)continue;
+           q.push(child);
+       }
+
+    }
+    return cntSame;
+}
+public:
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        //make graph ready
+        unordered_map<int,vector<int>>adj;
+        for(auto &vect:edges){
+            int u=vect[0];
+            int v=vect[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        //take  an ans vector
+        vector<int>ans(n,0);
+       //call bfs for every node
+       for(int i=0;i<n;i++){
+           int res=bfs(adj,i,labels[i],labels);
+           ans[i]=res;
+       }
+       //return ans
+       return ans;
+        
+    }
+};
