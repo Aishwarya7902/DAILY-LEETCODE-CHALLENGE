6f4b5a0421b5c784995a80c7da2d09dfa98ac6dc
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-12 07:00:06 -0800

    Update 12th jan__1519. Number of Nodes in the Sub-Tree With the Same Label.cpp

--- 2023/JANUARY/12th jan__1519. Number of Nodes in the Sub-Tree With the Same Label.cpp
+++ 2023/JANUARY/12th jan__1519. Number of Nodes in the Sub-Tree With the Same Label.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/description/
-VIDEO(MAZHAR BHAIYA):
+VIDEO(MAZHAR BHAIYA): https://www.youtube.com/watch?v=4seuiuW8c0o
 
 /*
 NAIVE SOLUTION :giving tle
@@ -48,6 +48,49 @@ public:
            int res=bfs(adj,i,labels[i],labels);
            ans[i]=res;
        }
+       //return ans
+       return ans;
+        
+    }
+};
+
+
+
+/*
+method 2
+*/
+
+class Solution {
+private:
+vector<int> dfs(unordered_map<int,vector<int>>&adj,int curr,int parent,vector<int>& ans,string &labels){
+    vector<int> my_count(26,0);
+    char mylabel=labels[curr];
+    my_count[mylabel-'a']=1;
+    for(int &child:adj[curr]){
+        if(child==parent)continue;
+        vector<int> child_count(26,0);
+         child_count=dfs(adj,child,curr,ans,labels);
+         for(int i=0;i<26;i++){
+             my_count[i]+=child_count[i];
+         }
+    }
+    ans[curr]=my_count[mylabel-'a'];
+    return my_count;
+}
+public:
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        //make graph ready
+        unordered_map<int,vector<int>>adj;
+        for(auto &vect:edges){
+            int u=vect[0];
+            int v=vect[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        //take  an ans vector
+        vector<int>ans(n,0);
+       //call dfs for every node
+       dfs(adj,0,-1,ans,labels);
        //return ans
        return ans;
         
