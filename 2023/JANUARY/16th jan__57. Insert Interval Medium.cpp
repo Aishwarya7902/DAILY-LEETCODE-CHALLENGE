136a2229af405714652b2a7ee5d76e6a5eed3139
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-16 04:48:37 -0800

    Update 16th jan__57. Insert Interval Medium.cpp

--- 2023/JANUARY/16th jan__57. Insert Interval Medium.cpp
+++ 2023/JANUARY/16th jan__57. Insert Interval Medium.cpp
@@ -36,3 +36,47 @@ public:
 
     }
 };
+
+/*
+OPTIMISATION
+TC:O(N)
+SC:O(N)
+*/
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i=0;
+        int n=intervals.size();
+        vector<vector<int>>ans;
+        while(i<n){
+            //condition 1 ...if end of current interval < start of newInterval ,it means newInterval kahin aage jake merge hoga so kuch ni karo bas tum aage badh jao
+            if(intervals[i][1]<newInterval[0]){
+             ans.push_back(intervals[i]);
+             
+            }
+            
+            //condition 2....if end of newInterval < start of current interval....it means newInterval ko merge karne ki bari  aa gai
+            else if(newInterval[1]<intervals[i][0]){
+                break;
+            }
+            //condition 3 ...overlap nikalne ki bari 
+            else{
+                newInterval[0]=min(newInterval[0],intervals[i][0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
+                //intervals.erase(intervals.begin()+i);
+
+            }
+            i++;
+            
+        }
+        ans.push_back(newInterval);
+        while(i<n){
+          ans.push_back(intervals[i]);
+           i++; 
+        }
+        return ans;
+
+    }
+};
