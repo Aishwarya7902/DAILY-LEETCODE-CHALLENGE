7722f62af33d945e3a60ece9da9ab687eb2005b7
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-16 04:22:39 -0800

    Create 16th jan__57. Insert Interval Medium.cpp

---
+++ 2023/JANUARY/16th jan__57. Insert Interval Medium.cpp
@@ -0,0 +1,38 @@
+PROBLEM:https://leetcode.com/problems/insert-interval/
+VIDEO (MAZHAR BHAIYA) : https://www.youtube.com/watch?v=XXaVIDIbUn4
+
+/*
+naive soln (TLE)
+tc:o(n^2)
+sc:o(1)
+*/
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i=0;
+        while(i<intervals.size()){
+            //condition 1 ...if end of current interval < start of newInterval ,it means newInterval kahin aage jake merge hoga so kuch ni karo bas tum aage badh jao
+            if(intervals[i][1]<newInterval[0])
+             i++;
+            
+            //condition 2....if end of newInterval < start of current interval....it means newInterval ko merge karne ki bari  aa gai
+            else if(newInterval[1]<intervals[i][0]){
+                intervals.insert(intervals.begin()+i,newInterval);
+                return intervals;
+            }
+            //condition 3 ...overlap karne ki bari
+            else{
+                newInterval[0]=min(newInterval[0],intervals[i][0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
+                intervals.erase(intervals.begin()+i);
+
+            }
+            
+        }
+        intervals.push_back(newInterval);
+        return intervals;
+
+    }
+};
