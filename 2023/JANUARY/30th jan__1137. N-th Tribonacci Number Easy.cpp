56abcafdee3b998d9f430bbccbd7efdfa589d52c
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-30 01:34:27 -0800

    Update 30th jan__1137. N-th Tribonacci Number Easy.cpp

--- 2023/JANUARY/30th jan__1137. N-th Tribonacci Number Easy.cpp
+++ 2023/JANUARY/30th jan__1137. N-th Tribonacci Number Easy.cpp
@@ -18,6 +18,25 @@ public:
     }
 };
 
+/*MEMOIZATION
+
+TC:
+SC:
+*/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0)return 0;
+        if(n==1 || n==2)return 1;
+       int dp[38]={0};
+       if(dp[n]!=0)return dp[n];
+       int ans=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+       
+       return dp[n]=ans;
+    }
+};
+
 /*
 DYNAMIC PROGRAMMING:
 tc:o(n)
