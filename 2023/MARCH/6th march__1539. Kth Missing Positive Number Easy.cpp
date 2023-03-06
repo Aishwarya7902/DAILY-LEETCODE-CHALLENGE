54d3a16308e8e21878e2d6934efa8a397b4868a9
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-03-05 22:36:28 -0800

    Update 6th march__1539. Kth Missing Positive Number Easy.cpp
    method 2

--- 2023/MARCH/6th march__1539. Kth Missing Positive Number Easy.cpp
+++ 2023/MARCH/6th march__1539. Kth Missing Positive Number Easy.cpp
@@ -4,6 +4,8 @@ VIDEO:
 /*
 METHOD 1
 PSEUDO CODE
+tc:o(n)
+sc:o(1)
 1.if all elements of array are greater than k ..it means k elements are already missing..hence return k
 2.if not ....k++
 3.return k
@@ -16,5 +18,26 @@ public:
            if(a<=k)k++;
        } 
        return k;
+    }
+};
+
+/*
+method 2
+tc:o(logn)
+sc:o(1)
+*/
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+       int lo=0,hi=arr.size();
+       while(lo<hi){
+           int mid=lo+(hi-lo)/2;
+           if(arr[mid]-mid>k)
+            hi=mid;
+           else
+            lo=mid+1;
+       }
+       return hi+k;
     }
 };
