e8d52ff70c2513b44824019fd506f0f57ea596e3
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-03-08 10:16:08 -0800

    Create 8th march__875. Koko Eating Bananas Medium.cpp

---
+++ 2023/MARCH/8th march__875. Koko Eating Bananas Medium.cpp
@@ -0,0 +1,43 @@
+PROBLEM:https://leetcode.com/problems/koko-eating-bananas/description/
+VIDEO:https://www.youtube.com/watch?v=QQcEIxK-snE
+
+/*
+
+*/
+
+class Solution {
+public:
+    
+    bool canEatAll(vector<int>& piles, int givenHour, int h) {
+        int actualHour = 0;
+        
+        for(int &x : piles) {
+            actualHour += x/givenHour;
+            
+            if(x%givenHour != 0)
+                actualHour++;
+            
+        }
+        
+        return actualHour <= h;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        
+        int l = 1, r = *max_element(begin(piles), end(piles));
+        
+        while(l < r) {
+            int mid = l + (r-l)/2;
+            
+            if(canEatAll(piles, mid, h)) {
+                r = mid;
+            } else {
+                l = mid+1;
+            }
+            
+        }
+        
+        return l;
+    }
+};
