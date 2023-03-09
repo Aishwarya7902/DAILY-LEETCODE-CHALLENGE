d653f10e4c96c818a183c89d22c452e09e8e8843
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-03-09 03:36:45 -0800

    Update 9th march__142. Linked List Cycle II Medium.cpp
    OPTIMISATION USING SLOW AND FAST POINTER

--- 2023/MARCH/9th march__142. Linked List Cycle II Medium.cpp
+++ 2023/MARCH/9th march__142. Linked List Cycle II Medium.cpp
@@ -24,7 +24,47 @@ public:
             }
             temp=temp->next;
         }
-        return NULL;
+        return NULL;   
+    }
+};
+
+/*
+method 2:
+*/
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL)return NULL;
+        //if head is null or LL has only one node...then cycle ka sawal hi ni uthta hai
+                ListNode *slow=head,*fast=head;
+        //now if slow and fast becomes same ...it means loop to hai
+                while(fast!=NULL && fast->next!=NULL){
+                    slow=slow->next;
+                    fast=fast->next->next;
+                    if(slow==fast)break;
+                }
         
+                //while loop breaks due to two conditions....i)slow=fast.....ii)fast/fast->next=null
+                if(slow!=fast)return NULL;
+                ListNode *entry=head;
+        //ab loop kaha hai..ye kaise paaata lagayen
+        /*
+        FOR PROOF WATCH VIDEO😂
+        slow ,fast ,covered distance 
+        slow=l1+l2
+        fast=l1+l2+nk
+        2slow=fast
+        2(l1+l2)=l1+l2+nk
+        l1=nk-l2
+        hence distance covered by entry and slow to reach start node will be same 
+        
+        */
+                while(entry!=slow){
+                    entry=entry->next;
+                    slow=slow->next;
+                    
+                }
+                return entry;
     }
 };
