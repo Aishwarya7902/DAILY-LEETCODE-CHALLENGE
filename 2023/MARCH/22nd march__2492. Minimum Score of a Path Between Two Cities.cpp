903d73ace5ce164a808cba586fbef3912bb1792a
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-03-22 04:08:50 -0700

    Update 22nd march__2492. Minimum Score of a Path Between Two Cities.cpp

--- 2023/MARCH/22nd march__2492. Minimum Score of a Path Between Two Cities.cpp
+++ 2023/MARCH/22nd march__2492. Minimum Score of a Path Between Two Cities.cpp
@@ -1,6 +1,6 @@
 PROBLEM:https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/description/
 BLOG:https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/solutions/3326775/image-explanation-both-bfs-dfs-approaches-c-java-python-union-find-is-overrated/?orderBy=most_votes
-VIDEO:
+VIDEO:https://www.youtube.com/watch?v=FFjo5137M-E
 
 /*
 METHOD 1
@@ -37,6 +37,42 @@ public:
               }
           }
       }
+      return ans; 
+    }
+};
+
+
+/*
+using DFS
+TC
+SC
+*/
+
+class Solution {
+public:
+    void dfs(int source, vector<vector<pair<int,int>>>& graph,int &ans,vector<bool>&vis){
+        vis[source]=1;
+        for(auto& [v,dis]:graph[source]){
+            ans=min(ans,dis);
+            if(!vis[v])
+             dfs(v,graph,ans,vis);
+        }
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        
+      //to store ans
+      int ans=INT_MAX;
+      //make graph
+      vector<vector<pair<int,int>>>graph(n+1);
+      for(auto edge:roads){
+          graph[edge[0]].push_back({edge[1],edge[2]});
+           graph[edge[1]].push_back({edge[0],edge[2]});
+      } 
+      //dfs
+      vector<bool>vis(n+1,0);
+      queue<int>q;
+      dfs(1,graph,ans,vis);
+      
       return ans; 
     }
 };
