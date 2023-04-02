ed1de58c9d559baeb1317ec2aa63e4e12f5d58e0
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-01 23:55:52 -0700

    Update 2nd april__2300. Successful Pairs of Spells and Potions Medium.cpp
    binary search

--- 2023/APRIL/2nd april__2300. Successful Pairs of Spells and Potions Medium.cpp
+++ 2023/APRIL/2nd april__2300. Successful Pairs of Spells and Potions Medium.cpp
@@ -36,3 +36,39 @@ public:
        return ans; 
     }
 };
+
+/*
+method 2
+for finding how many potions can contribute toward success
+use binary search for that
+sort potions array
+for eg if jth potion contributes towards success...then starting from j till last index all will
+contribute
+*/
+
+class Solution {
+public:
+
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+
+       int n=spells.size();
+       int m=potions.size();
+       sort(potions.begin(),potions.end());
+       vector<int>ans(n);
+       for(int i=0;i<n;i++){
+           int val=spells[i];
+           int lo=0,hi=m-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            long long prod=long(val)*long(potions[mid]);
+            if(prod>=success)
+             hi=mid-1;
+            else
+             lo=mid+1;
+        }
+        ans[i]=m-lo;
+       }
+       return ans; 
+    }
+};
+
