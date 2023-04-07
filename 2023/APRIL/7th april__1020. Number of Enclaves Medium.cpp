c8e8c2730d45c7b50d9675f35d1a8f3300360ed4
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-06 23:25:20 -0700

    Create 7th april__1020. Number of Enclaves Medium.cpp

---
+++ 2023/APRIL/7th april__1020. Number of Enclaves Medium.cpp
@@ -0,0 +1,66 @@
+problem:https://leetcode.com/problems/number-of-enclaves/description/
+blog:
+video:
+
+/*
+self
+pseudo code
+simple hai
+1.we cant move on the land of the edges..........so unka koi use ni..........hence mark them as water/sea
+2.now simply run a dfs ......which will return no of land cells corresponding to that dfs call
+3.add the returned value in final ans
+*/
+
+class Solution {
+public:
+    //dfs
+    int  dfs(int i,int j,vector<vector<int>>& grid,int &count){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1)
+         return 0;
+        grid[i][j]=0;
+        count++;
+        dfs(i-1,j,grid,count);
+        dfs(i+1,j,grid,count);
+        dfs(i,j-1,grid,count);
+        dfs(i,j+1,grid,count);
+        return count;
+    }
+    //dfsEdge
+    void dfsEdge(int i,int j,vector<vector<int>>& grid){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1)
+         return ;
+        grid[i][j]=0;
+        dfsEdge(i-1,j,grid);
+        dfsEdge(i+1,j,grid);
+        dfsEdge(i,j-1,grid);
+        dfsEdge(i,j+1,grid);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+       int m=grid.size();
+       int n=grid[0].size();
+       for(int i=0;i<m;i++){
+           dfsEdge(i,0,grid);
+           dfsEdge(i,n-1,grid);
+
+       } 
+       for(int j=0;j<n;j++){
+           dfsEdge(0,j,grid);
+           dfsEdge(m-1,j,grid);
+
+       } 
+       int cnt=0;
+       for(int i=1;i<m-1;i++){
+           for(int j=1;j<n-1;j++){
+               if(grid[i][j]==1){
+                   int count=0;
+                   cnt+=dfs(i,j,grid,count);
+               }
+           }
+       }
+       return cnt;
+    }
+};
