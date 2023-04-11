b07083e49cf6925603410f24ea1ad92af5699562
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-11 00:08:24 -0700

    Update 11th April__2390. Removing Stars From a String Medium.cpp

--- 2023/APRIL/11th April__2390. Removing Stars From a String Medium.cpp
+++ 2023/APRIL/11th April__2390. Removing Stars From a String Medium.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/removing-stars-from-a-string/description/
-VIDEO:
+VIDEO:(mazhar bhaiya) https://www.youtube.com/watch?v=s1pcdsRCVDg
 BLOG:
 
 /*
@@ -64,4 +64,56 @@ public:
 };
 
 /*
-method
+method 2
+using a vector 
+vector behaves like a stack and there is no need to reverse the string at the end
+for clarity see video(mazhar bhaiya)
+*/
+
+
+class Solution {
+public:
+    string removeStars(string s) {
+      if(s.size()<=1)return s;
+      string st="";
+
+      for(int i=0;i<s.size();i++){
+          if(s[i]=='*')
+           st.pop_back();
+          else
+            st.push_back(s[i]);
+      }  
+      
+      return st;
+    }
+};
+
+/*
+method 3
+using two pointer
+for clarity watch video
+*/
+
+class Solution {
+public:
+    string removeStars(string s) {
+     int n=s.size() ;
+     vector<char>temp(n);
+     int j=0;
+     for(int i=0;i<n;i++){
+         if(s[i]=='*')
+          j--;
+        else{
+            temp[j]=s[i];
+            j++;
+
+        }
+     }
+     string ans="";
+     for(int k=0;k<=j-1;k++){
+         ans.push_back(temp[k]);
+     }
+     return ans;
+
+    }
+};
