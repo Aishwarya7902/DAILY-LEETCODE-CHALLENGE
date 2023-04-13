8cdc7171a8cba06463cb062d41e6c123c8f9fa18
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-13 02:20:48 -0700

    Update 13th April_946. Validate Stack Sequences Medium.cpp

--- 2023/APRIL/13th April_946. Validate Stack Sequences Medium.cpp
+++ 2023/APRIL/13th April_946. Validate Stack Sequences Medium.cpp
@@ -1,5 +1,5 @@
 problem:https://leetcode.com/problems/validate-stack-sequences/description/
-video:
+video:(mazhar bhaiya) https://www.youtube.com/watch?v=HEh7EtNSXtg
 blog:
 /*
 self
@@ -35,5 +35,31 @@ public:
            }
        } 
        return true; 
+    }
+};
+
+/*
+BETTER CODE
+TC:O(M)
+SC:O(M)
+*/
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+
+       stack<int>st;
+       int m=pushed.size();
+       int i=0,j=0;
+
+       while(i<m && j<m){
+           st.push(pushed[i]);
+           while(!st.empty() &&  j<m && st.top()==popped[j]){
+               st.pop();
+               j++;
+           }
+           i++;
+       }
+       return st.empty();
     }
 };
