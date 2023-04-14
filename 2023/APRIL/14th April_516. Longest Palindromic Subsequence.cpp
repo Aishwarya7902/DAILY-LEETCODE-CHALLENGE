5beb370723a2c597cf56b5b2aaebf7ed8e150dc8
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-14 02:32:24 -0700

    Update 14th April_516. Longest Palindromic Subsequence.cpp

--- 2023/APRIL/14th April_516. Longest Palindromic Subsequence.cpp
+++ 2023/APRIL/14th April_516. Longest Palindromic Subsequence.cpp
@@ -47,4 +47,36 @@ public:
     }
 };
 
+/*
+BOTTOM UP
+*/
+
+class Solution {
+public:
+    
+    int lcs(string &s1,string &s2){
+      int m=s1.size();
+      int n=s2.size();
+      vector<vector<int>> dp(m+1,vector<int>(n+1));
+      for(int i=0;i<m+1;i++){
+          for(int j=0;j<n+1;j++){
+              if(i==0 || j==0)
+               dp[i][j]=0;
+           else if(s1[i-1]==s2[j-1])
+             dp[i][j]=1+dp[i-1][j-1];
+             else
+              dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+          }
+      }  
+      return dp[m][n];
+        
+    }
+    int longestPalindromeSubseq(string s) {
+        string s2=s;
+        reverse(s2.begin(),s2.end());
+       
+        return lcs(s,s2);
+    }
+};
+
 
