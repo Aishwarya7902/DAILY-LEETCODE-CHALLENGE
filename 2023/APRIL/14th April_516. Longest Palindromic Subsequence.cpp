a22c4ea7620f9d0b7c4d695fc6e93f97321a9b6b
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-14 02:40:40 -0700

    Update 14th April_516. Longest Palindromic Subsequence.cpp

--- 2023/APRIL/14th April_516. Longest Palindromic Subsequence.cpp
+++ 2023/APRIL/14th April_516. Longest Palindromic Subsequence.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/longest-palindromic-subsequence/description/
-VIDEO:
+VIDEO:https://www.youtube.com/watch?v=M4pfB3G-YQc
 BLOG:
 
 /*
@@ -76,6 +76,31 @@ public:
         reverse(s2.begin(),s2.end());
        
         return lcs(s,s2);
+    }
+};
+
+/*
+method 4
+*/
+class Solution {
+public:
+    int t[1001][1001];
+    int solve(string &s,int i,int j){
+        if(i>j)return 0;
+        if(i==j)return 1;
+        if(t[i][j]!=-1)return t[i][j];
+        if(s[i]==s[j]){
+            return t[i][j]=2+solve(s,i+1,j-1);
+
+        }
+        return t[i][j]=max(solve(s,i+1,j),solve(s,i,j-1));
+
+
+    }
+   
+    int longestPalindromeSubseq(string s) {
+        memset(t,-1,sizeof(t));
+        return solve(s,0,s.size()-1);
     }
 };
 
