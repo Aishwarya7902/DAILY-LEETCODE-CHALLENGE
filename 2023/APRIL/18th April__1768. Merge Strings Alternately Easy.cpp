53f7f09de81270fe1c028cfad9ecaae020bf4530
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-17 23:10:12 -0700

    Update 18th April__1768. Merge Strings Alternately Easy.cpp

--- 2023/APRIL/18th April__1768. Merge Strings Alternately Easy.cpp
+++ 2023/APRIL/18th April__1768. Merge Strings Alternately Easy.cpp
@@ -7,7 +7,7 @@ BLOG:
 
 SELF
 TC:O(M+N)
-SC:O(1)
+SC:O(M+N)
 */
 
 class Solution {
@@ -34,3 +34,27 @@ public:
 
     }
 };
+/*
+BETTER CODE
+TC:O(M+N)
+SC:O(M+N)
+*/
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        int m=word1.size();
+        int n=word2.size();
+        //initialize i--->word1, j--->word2
+        int i=0,j=0;
+        while(i<m || j<n){
+            if(i<m)ans.push_back(word1[i++]);
+           if(j<n) ans.push_back(word2[j++]);
+
+        }
+        return ans;
+         
+    }
+};
+
