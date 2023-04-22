3466d50f2bdb796100462e7a30a1b5fd859fbe5f
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-22 01:52:10 -0700

    Update 22nd April__1312. Minimum Insertion Steps to Make a String Palindrome.cpp

--- 2023/APRIL/22nd April__1312. Minimum Insertion Steps to Make a String Palindrome.cpp
+++ 2023/APRIL/22nd April__1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -74,3 +74,45 @@ public:
         
     }
 };
+
+/*
+BOTTOM UP
+TC:O(N^2)
+SC:O(N^2)
+*/
+
+class Solution {
+public:
+
+// LPS FUNCTION
+    int longestPalindromicSubsequence(string &s){
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int len=1;len<=n;len++){
+            for(int start=0;start<=(n-len);start++){
+                int end=len+start-1;
+                //[start,end] string under consideration
+                if(len==1){
+                    dp[start][end]=1;
+                    continue;
+
+                }
+                if(s[start]==s[end]){
+                    dp[start][end]=2+dp[start+1][end-1];
+                }
+                else{
+                   dp[start][end]=max(dp[start+1][end],dp[start][end-1]); 
+                }
+
+            }
+        }
+        return dp[0][n-1];
+    }
+    int minInsertions(string s) {
+        int n=s.size();
+        return n-longestPalindromicSubsequence(s);
+        
+    }
+};
+
+
