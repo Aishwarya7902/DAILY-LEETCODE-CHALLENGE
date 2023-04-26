64548470b1053fc71079cc4e68197dda30976a97
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-25 22:19:34 -0700

    Update 26th April__258. Add Digits Easy.cpp

--- 2023/APRIL/26th April__258. Add Digits Easy.cpp
+++ 2023/APRIL/26th April__258. Add Digits Easy.cpp
@@ -27,3 +27,45 @@ public:
         return addDigits(currSum);
     }
 };
+
+
+/*
+cleaner code
+*/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while(num){
+            int rem=num%10;
+            sum+=rem;
+            num/=10;
+        }
+        if(sum<10)return sum;
+        else
+         return addDigits(sum);
+    }
+};
+
+/*
+ITERATIVE METHOD
+*/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while(num>9){
+           while(num){
+            int rem=num%10;
+            sum+=rem;
+            num/=10;
+            }
+            num=sum;
+            sum=0;
+        }
+        return num;
+        
+    }
+};
