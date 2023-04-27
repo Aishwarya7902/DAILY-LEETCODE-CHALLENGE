15d7747b3d3be8535afcdd80b9171f3e73e645a1
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-04-26 23:47:14 -0700

    Create 27th April__319. Bulb Switcher Medium.cpp

---
+++ 2023/APRIL/27th April__319. Bulb Switcher Medium.cpp
@@ -0,0 +1,15 @@
+problem:https://leetcode.com/problems/bulb-switcher/description/
+video :
+(aryan mittal):https://www.youtube.com/watch?v=byFPYruQl2s
+
+/*
+tc:
+sc:
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return int(sqrt(n));   
+    }
+};
