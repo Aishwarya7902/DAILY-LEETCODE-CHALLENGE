4721f50153354c5798bc691e97b5771a76c9a83e
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-05-01 10:21:30 -0700

    Create 1st may__1491. Average Salary Excluding the Minimum and Maximum Salary.cpp

---
+++ 2023/MAY/1st may__1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,22 @@
+PROBLEM:https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/description/
+
+/*
+TC:
+SC:
+*/
+
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int n=salary.size();
+        double sum=0;
+        sort(salary.begin(),salary.end());
+        for(int i=1;i<n-1;i++){
+            sum+=salary[i];
+        }
+        return sum/(n-2);
+
+
+        
+    }
+};
