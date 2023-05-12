15f98910fe4e7cde93528c86e0bdd3b6ad89473f
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-05-12 11:46:25 -0700

    Update 12th may__2140. Solving Questions With Brainpower Medium.cpp

--- 2023/MAY/12th may__2140. Solving Questions With Brainpower Medium.cpp
+++ 2023/MAY/12th may__2140. Solving Questions With Brainpower Medium.cpp
@@ -25,3 +25,26 @@ public:
        return solve(0,questions,dp); 
     }
 };
+
+/*
+bottom up
+tc
+sc
+*/
+long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        
+        if(n == 1)
+            return questions[0][0];
+        
+        vector<long long> t(200001);
+        //t[i] = Max points gained by Questions from questions[i to n-1]
+        //return t[0] - from 0 to n-1
+
+        
+        for(int i = n-1; i >= 0; i--) {
+            t[i] = max(questions[i][0] + t[i + questions[i][1] + 1], t[i+1]);
+        }
+        
+        return t[0];
+    }
