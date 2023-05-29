abe3f6fba229c48ec6a7b7364205cf6ec46fbe7d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-05-28 22:37:52 -0700

    Create 29th may__1603. Design Parking System Easy.cpp

---
+++ 2023/MAY/29th may__1603. Design Parking System Easy.cpp
@@ -0,0 +1,45 @@
+PROBLEM: https://leetcode.com/problems/design-parking-system/description/
+VIDEO :
+
+/*
+SELF
+TC:O(1)
+SC:O(1)
+*/
+
+
+class ParkingSystem {
+public:
+    int b,m,s;
+    int currBig=0,currMed=0,currSmall=0;
+    ParkingSystem(int big, int medium, int small) {
+        b=big;
+        m=medium;
+        s=small;
+        
+    }
+    
+    bool addCar(int carType) {
+        if(carType==1){
+            if(currBig<b){
+                currBig++;
+                return true;
+            }
+        }
+        else if(carType==2){
+            if(currMed<m){
+                currMed++;
+                return true;
+            }
+        }
+        else if(carType==3){
+            if(currSmall<s){
+                currSmall++;
+                return true;
+            }
+        }
+
+
+     return false;   
+    }
+};
