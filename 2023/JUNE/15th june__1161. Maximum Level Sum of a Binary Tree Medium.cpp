a39463587acb0aa926a4f73d73fe36c9ce086524
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-06-15 09:40:55 -0700

    Create 15th june__1161. Maximum Level Sum of a Binary Tree Medium.cpp

---
+++ 2023/JUNE/15th june__1161. Maximum Level Sum of a Binary Tree Medium.cpp
@@ -0,0 +1,35 @@
+PROBLEM:https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/description/
+VIDEO:
+
+/*
+TC:O(N)
+SC:O(1)
+*/
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int level=0;
+        int ans;
+       int  max_val=INT_MIN;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int sum=0;
+            int n=q.size();
+            while(n--){
+                TreeNode* node=q.front();
+                q.pop();
+                sum+=node->val;
+                if(node->right)q.push(node->right);
+                if(node->left)q.push(node->left);
+            }
+            level++;
+            if(sum>max_val){
+                ans=level;
+                max_val=sum;
+            }
+        }
+        return ans;
+    }
+};
