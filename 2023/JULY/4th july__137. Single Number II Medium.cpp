26a5c6dcf604200e660cb5b77aa2684eb68774bb
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-07-04 04:30:22 -0700

    Update 4th july__137. Single Number II Medium.cpp

--- 2023/JULY/4th july__137. Single Number II Medium.cpp
+++ 2023/JULY/4th july__137. Single Number II Medium.cpp
@@ -30,3 +30,20 @@ public:
 optimisation
 
 video (ARYAN MITTAL ) :https://www.youtube.com/watch?v=7xJJ_42P_0U
+
+tc:o(n)
+sc:o(1)
+
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones=0,twos=0;
+        for(int i=0;i<nums.size();i++){
+            ones=(ones^nums[i]) & ~twos;
+            twos=(twos^nums[i]) & ~ones;
+        }
+        return ones;
+    }
+};
