e038c090b3d7d0d791407f3684e5646782860492
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-07-05 02:44:09 -0700

    Update 5th july__1493. Longest Subarray of 1's After Deleting One Element.cpp

--- 2023/JULY/5th july__1493. Longest Subarray of 1's After Deleting One Element.cpp
+++ 2023/JULY/5th july__1493. Longest Subarray of 1's After Deleting One Element.cpp
@@ -43,3 +43,31 @@ public:
         
     }
 };
+
+/*
+optimisation
+
+tc:o(n)
+sc:o(1)
+
+  */
+
+class Solution {
+public:
+    int n;
+    int longestSubarray(vector<int>& nums) {
+       n=nums.size();
+       int maxLen=0;
+       int countZero=0;
+       int i=0;
+       for(int j=0;j<n;j++){
+           if(nums[j]==0)countZero++;
+           while(countZero>1){
+               if(nums[i]==0)countZero--;
+               i++;
+           }
+           maxLen=max(maxLen,j-i);
+       } 
+       return maxLen;
+    }
+};
