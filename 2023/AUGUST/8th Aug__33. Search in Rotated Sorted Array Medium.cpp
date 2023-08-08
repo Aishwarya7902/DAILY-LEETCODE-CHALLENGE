2b2d02c09ac38a52e0de7b0b404f08f8b315f4dd
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-08-08 02:10:26 -0700

    Update 8th Aug__33. Search in Rotated Sorted Array Medium.cpp

--- 2023/AUGUST/8th Aug__33. Search in Rotated Sorted Array Medium.cpp
+++ 2023/AUGUST/8th Aug__33. Search in Rotated Sorted Array Medium.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/search-in-rotated-sorted-array/description/
-VIDEO:
+VIDEO:https://www.youtube.com/watch?v=U1VsdRgVevY
 
 /*
   BRUTE FORCE
@@ -16,6 +16,59 @@ public:
             if(nums[i]==target)return i;
         }
         return -1;
+        
+    }
+};
+
+/*
+optimisation USING binary search
+TC:
+*/
+
+class Solution {
+public:
+    int n;
+    //helper 1
+    int binarySearch(int l,int r,vector<int>& nums,int target){
+        int indx=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                indx=mid;
+                break;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return indx;
+    }
+    //helper 2
+    int findPivot(vector<int>& nums){
+        int l=0,r=n-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[r]){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return r;
+
+    }
+    int search(vector<int>& nums, int target) {
+        n=nums.size();
+        int pivot_index=findPivot(nums);
+        int indx=binarySearch(0,pivot_index-1,nums,target);
+        if(indx!=-1)
+         return indx;
+        indx=binarySearch(pivot_index,n-1,nums,target);
+        return indx;
         
     }
 };
