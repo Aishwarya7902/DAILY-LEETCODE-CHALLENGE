93b6356490235547ff9bf9952a457b01c1e336ae
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-08-11 05:42:07 -0700

    Update 11th Aug__518. Coin Change II Medium.cpp

--- 2023/AUGUST/11th Aug__518. Coin Change II Medium.cpp
+++ 2023/AUGUST/11th Aug__518. Coin Change II Medium.cpp
@@ -40,3 +40,47 @@ public:
         return solve(0,amount,coins);
     }
 };
+
+
+/*
+memoization
+tc:o(indx*amount)
+sc:
+*/
+
+class Solution {
+public:
+    int n;
+    int t[301][5001];
+    //helper
+
+    int solve(int indx,int amount, vector<int>& coins){
+        //base case1
+        if(amount==0){
+            return 1;
+        }
+        //base case2
+
+        if(indx>=n)
+        {
+             return 0;
+        }
+
+        if(t[indx][amount]!=-1)
+         return t[indx][amount];
+        //base case3
+
+        if(coins[indx]>amount)
+         {
+             return t[indx][amount]=solve(indx+1,amount,coins);
+         }
+        int taken=solve(indx,amount-coins[indx],coins);
+        int not_taken=solve(indx+1,amount,coins);
+        return t[indx][amount]=(taken+not_taken);
+    }
+    int change(int amount, vector<int>& coins) {
+        n=coins.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,amount,coins);
+    }
+};
