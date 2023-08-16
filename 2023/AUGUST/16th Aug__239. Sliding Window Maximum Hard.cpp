e7c4a46e8ed07b329ffb1c152403db6d491a9adf
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-08-16 02:08:09 -0700

    Create 16th Aug__239. Sliding Window Maximum Hard.cpp

---
+++ 2023/AUGUST/16th Aug__239. Sliding Window Maximum Hard.cpp
@@ -0,0 +1,37 @@
+PROBLEM :https://leetcode.com/problems/sliding-window-maximum/description/
+VIDEO(MIK BHAIYA):
+
+/*
+  SLIDING WINDOW
+  TC:
+  SC:
+*/
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        int i=0,j=0;
+        vector<int>ans;
+        deque<int>dq;
+        while(j<n){
+            while(!dq.empty() && nums[j]>nums[dq.back()]){
+                dq.pop_back();
+            }
+
+            dq.push_back(j);
+            if(j-i+1<k)j++;
+            else if(j-i+1==k){
+                ans.push_back(nums[dq.front()]);
+                if(nums[i]==nums[dq.front()]){
+                    dq.pop_front();
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+
+    }
+};
