bd52bef473aeba1a79e6cc9089b70a3329086c92
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-09-19 05:40:39 -0700

    Update 19th sept__287. Find the Duplicate Number Medium.cpp

--- 2023/SEPTEMBER/19th sept__287. Find the Duplicate Number Medium.cpp
+++ 2023/SEPTEMBER/19th sept__287. Find the Duplicate Number Medium.cpp
@@ -23,3 +23,24 @@ public:
         return-1;
     }
 };
+
+
+/*
+  method 2
+  using map
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int &n:nums){
+            mp[n]++;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]>1)return nums[i];
+        }
+        return -1;
+    }
+};
