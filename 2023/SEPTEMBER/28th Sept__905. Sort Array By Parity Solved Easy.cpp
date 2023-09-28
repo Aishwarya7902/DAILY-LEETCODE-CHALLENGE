1ea9b4832dad284eb2a7abfeabecdc83fc55185a
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-09-28 09:49:23 -0700

    Update 28th Sept__905. Sort Array By Parity Solved Easy.cpp

--- 2023/SEPTEMBER/28th Sept__905. Sort Array By Parity Solved Easy.cpp
+++ 2023/SEPTEMBER/28th Sept__905. Sort Array By Parity Solved Easy.cpp
@@ -1,6 +1,6 @@
 PROBLEM:https://leetcode.com/problems/sort-array-by-parity/description/?envType=daily-question&envId=2023-09-28
 VIDEO:
-
+//method 1 (on own)
 //tc:o(n)
 //sc:o(1)
 class Solution {
@@ -25,3 +25,44 @@ public:
         return result;
     }
 };
+
+
+/*
+method 2
+tc:o(nlogn)
+sc:o(1)
+*/
+
+class Solution {
+public:
+    static bool comparator(int &a,int &b){
+        return a%2<b%2;
+    }
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(begin(nums),end(nums),comparator);
+        return nums;
+    }
+};
+
+/*
+method 3:
+tc:o(n)
+sc:o(1)
+ */
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        return nums;
+    }
+};
+
+
+    
