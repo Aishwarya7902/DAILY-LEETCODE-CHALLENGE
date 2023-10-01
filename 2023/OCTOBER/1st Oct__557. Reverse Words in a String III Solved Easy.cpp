366e6e4906281d917bd33986d3f4a7dfb756381d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-10-01 09:53:26 -0700

    Update 1st Oct__557. Reverse Words in a String III Solved Easy.cpp

--- 2023/OCTOBER/1st Oct__557. Reverse Words in a String III Solved Easy.cpp
+++ 2023/OCTOBER/1st Oct__557. Reverse Words in a String III Solved Easy.cpp
@@ -25,3 +25,24 @@ public:
         return s;
     }
 };
+
+
+/*
+Method 2
+using tokenization
+*/
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string token="";
+        string result="";
+        while(ss>>token){
+            reverse(begin(token),end(token));
+            result+=token+" ";
+        }
+        return result.substr(0,result.length()-1);
+    }
+};
