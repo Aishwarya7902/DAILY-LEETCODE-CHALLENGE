01da9edb8dd11a08f82fd0c0d992fd60e9637e62
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-10-17 07:07:05 -0700

    Update 16th Oct__119. Pascal's Triangle II.cpp

--- 2023/OCTOBER/16th Oct__119. Pascal's Triangle II.cpp
+++ 2023/OCTOBER/16th Oct__119. Pascal's Triangle II.cpp
@@ -20,3 +20,24 @@ public:
         return result[rowIndex];
     }
 };
+
+/*
+space optimisation
+tc:o(rowIndex*rowIndex)
+sc:o(rowIndex) 
+*/
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int>prev;
+        for(int i=0;i<rowIndex+1;i++){
+           vector<int>curr(i+1,1);
+            for(int j=1;j<i;j++){
+                curr[j]= prev[j]+ prev[j-1];
+            }
+            prev=curr;
+        }
+        return prev;
+    }
+};
