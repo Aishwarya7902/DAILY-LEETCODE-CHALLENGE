202b35b622991e05ad7f63cae419afa8432462b2
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-10-27 12:04:40 -0700

    Create 5. Longest Palindromic Substring Solved Medium.cpp

---
+++ 2023/OCTOBER/5. Longest Palindromic Substring Solved Medium.cpp
@@ -0,0 +1,33 @@
+PROBLEM:https://leetcode.com/problems/longest-palindromic-substring/description/?envType=daily-question&envId=2023-10-27
+VIDEO(MIK BHAIYA):https://www.youtube.com/watch?v=n_kL8BkURVA
+
+
+/*
+  RECURSION
+  */
+
+class Solution {
+public:
+    //function to check palindrome
+    bool solve(string &s,int i,int j){
+        if(i>=j)return true;
+        if(s[i]==s[j])return solve(s,i+1,j-1);
+        return false;
+    }
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int maxLen=INT_MIN;
+        int sp=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)){
+                    if(j-i+1>maxLen){
+                        maxLen=j-i+1;
+                        sp=i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp,maxLen);
+    }
+};
