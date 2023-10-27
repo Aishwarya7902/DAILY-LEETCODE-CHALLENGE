a6087496405a34c553420e788db4bfeda056cf40
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-10-27 12:20:48 -0700

    Update 27th Oct__5. Longest Palindromic Substring Solved Medium.cpp

--- 2023/OCTOBER/27th Oct__5. Longest Palindromic Substring Solved Medium.cpp
+++ 2023/OCTOBER/27th Oct__5. Longest Palindromic Substring Solved Medium.cpp
@@ -31,3 +31,41 @@ public:
         return s.substr(sp,maxLen);
     }
 };
+
+
+/*
+
+memoization
+tc:
+sc:
+*/
+
+class Solution {
+public:
+    //function to check palindrome
+    int t[1001][1001];
+    bool solve(string &s,int i,int j){
+        if(i>=j)return 1;
+        if(t[i][j]!=-1)return t[i][j];
+        if(s[i]==s[j])return t[i][j]=solve(s,i+1,j-1);
+        return t[i][j]=0;
+    }
+    string longestPalindrome(string s) {
+        memset(t,-1,sizeof(t));
+        int n=s.size();
+        int maxLen=INT_MIN;
+        int sp=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)){
+                    if(j-i+1>maxLen){
+                        maxLen=j-i+1;
+                        sp=i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp,maxLen);
+    }
+};
+
