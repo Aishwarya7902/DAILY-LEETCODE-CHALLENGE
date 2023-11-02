39f86dfef184e28440e22e7e1d538f128dc1019e
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-11-02 13:25:48 -0700

    Create 2nd Nov__2265. Count Nodes Equal to Average of Subtree Solved Medium.cpp

---
+++ 2023/NOVEMBER/2nd Nov__2265. Count Nodes Equal to Average of Subtree Solved Medium.cpp
@@ -0,0 +1,6 @@
+PROBLEM:https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/description/?envType=daily-question&envId=2023-11-02
+VIDEO(MIK BHAIYA):https://www.youtube.com/watch?v=Y4UGaLpSqCg
+
+/*
+  TC:O(N) //since we are visiting each node once
+  
