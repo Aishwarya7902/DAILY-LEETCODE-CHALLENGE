afa63227864c7902c3d79a54230305c376d2d1fa
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-11-13 10:17:16 -0800

    Create 13th Nov__2785. Sort Vowels in a String Solved Medium.cpp

---
+++ 2023/NOVEMBER/13th Nov__2785. Sort Vowels in a String Solved Medium.cpp
@@ -0,0 +1,42 @@
+PROBLEM:https://leetcode.com/problems/sort-vowels-in-a-string/description/?envType=daily-question&envId=2023-11-13
+VIDEO(MIK BHAIYA):https://www.youtube.com/watch?v=nEDXPkGsjXo
+
+/*
+  SOLVED ON OWN
+  TC:
+  SC:
+
+*/
+
+class Solution {
+public:
+   bool isVowel(char x){
+       if(x=='a' || x=='A' ||x=='e'||x=='E' || x=='i' ||x=='I' ||x=='o' ||x=='O' ||x=='u' ||x=='U')return true;
+
+       return false;
+   }
+    string sortVowels(string s) {
+        string curr;
+        string t;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(!isVowel(s[i])){
+                t+=s[i];
+            }
+            else{
+                t+='.';
+                curr+=s[i];
+            }
+        }
+
+        sort(begin(curr),end(curr));
+        int j=0;
+        for(int i=0;i<t.size();i++){
+            if(t[i]=='.'){
+                t[i]=curr[j];
+                j++;
+            }
+        }
+     return t;
+    }
+};
