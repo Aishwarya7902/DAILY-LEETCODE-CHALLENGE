c5129c56453cbe9872a43e0e23e3f2dad2fc6301
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-11-16 11:39:48 -0800

    Update 16th Nov__1980. Find Unique Binary String Medium.cpp

--- 2023/NOVEMBER/16th Nov__1980. Find Unique Binary String Medium.cpp
+++ 2023/NOVEMBER/16th Nov__1980. Find Unique Binary String Medium.cpp
@@ -1,9 +1,29 @@
 PROBLEM:https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2023-11-16
-VIDEO(MIK BHAIYA):
+VIDEO(MIK BHAIYA):https://www.youtube.com/watch?v=8Cay8q3BOx4
 
 /*
-  TC
-  SC
+  TC:O(n^2)
+  SC:o(n)
 
   */
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        unordered_set<int>st;
+        for(string &num:nums){
+            st.insert(stoi(num,0,2));
+        }
+
+        int n=nums.size();
+        string res="";
+        for(int i=0;i<=n;i++){
+            if(st.find(i)==st.end()){
+                res=bitset<16>(i).to_string();
+                return res.substr(16-n,n);
+            }
+        }
+        return "";
+    }
+};
 
