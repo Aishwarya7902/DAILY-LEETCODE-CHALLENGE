eac0ce3b33d81f9a9dfbb17fcc3f5215177dc551
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-11-18 05:38:05 -0800

    Update 18th Nov__1838. Frequency of the Most Frequent Element Medium.cpp

--- 2023/NOVEMBER/18th Nov__1838. Frequency of the Most Frequent Element Medium.cpp
+++ 2023/NOVEMBER/18th Nov__1838. Frequency of the Most Frequent Element Medium.cpp
@@ -90,3 +90,65 @@ public:
         
     }
 };
+
+  //Approach-2 (Using sliding window)
+//T.C : O(nlogn)
+//S.C : O(1)
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(begin(nums),end(nums));
+        int n=nums.size();
+        int result=0;
+        int l=0;
+        long currSum=0;
+        for(int r=0;r<n;r++){
+            long target=nums[r];
+            currSum+=nums[r];
+            // long windowSum=(r-l+1)*target;
+            // long opt=windowSum-currSum;
+            while((r-l+1)*target-currSum>k){
+                currSum-=nums[l];
+                l++;
+            }
+
+            result=max(result,r-l+1);
+        }
+        return result;
+    }
+};
+
+//Approach-3 (Improved sliding window)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums));
+        
+        int n = nums.size();
+        
+        int result = 0;
+        
+        int i = 0;
+        long currSum = 0;
+        
+        for(int j = 0; j<n; j++) {
+            
+            long target = nums[j];
+            currSum   += nums[j];
+            
+            if((j-i+1)*target - currSum > k) {
+                currSum -= nums[i];
+                i++;
+            }
+            
+            result = max(result, j-i+1);
+            
+        }
+        return result;
+    }
+};
+
+  
