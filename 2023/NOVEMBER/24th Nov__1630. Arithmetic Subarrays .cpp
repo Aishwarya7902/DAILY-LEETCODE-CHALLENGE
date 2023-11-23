fe349f22f386f58d2ae23ee7c1da88f4904e233c
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-11-23 11:16:26 -0800

    Update 24th Nov__1630. Arithmetic Subarrays .cpp

--- 2023/NOVEMBER/24th Nov__1630. Arithmetic Subarrays .cpp
+++ 2023/NOVEMBER/24th Nov__1630. Arithmetic Subarrays .cpp
@@ -29,3 +29,45 @@ public:
         return result;
     }
 };
+
+/*  OPTIMISATION
+    TC:O(M*N)
+    SC:O(N)
+*/
+
+class Solution {
+public:
+    bool check(vector<int>& arr){
+        int n=arr.size();
+        int maxE=INT_MIN;
+        int minE=INT_MAX;
+        unordered_set<int>st;
+        for(int &num:arr){
+            minE=min(minE,num);
+            maxE=max(maxE,num);
+            st.insert(num);
+        }
+
+        if((maxE-minE)%(n-1)!=0)return false;
+        int d=(maxE-minE)/(n-1); //tn=a+(n-1)d
+        int curr=minE;
+        while(curr<maxE){
+            if(st.find(curr)==st.end())return false;
+            curr+=d;
+        }
+        return true;
+    }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        int  m=l.size();
+        vector<bool>result;
+        for(int i=0;i<m;i++){
+            int start=l[i];
+            int end=r[i];
+            vector<int>arr(begin(nums)+start,begin(nums)+end+1);
+            bool isAp=check(arr);
+            result.push_back(isAp);
+
+        }
+        return result;
+    }
+};
