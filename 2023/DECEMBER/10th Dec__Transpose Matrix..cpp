1963002c12fdd003fc1841dc48a54c2895d1355a
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-10 07:01:23 -0800

    Update 10th Dec__Transpose Matrix..cpp

--- 2023/DECEMBER/10th Dec__Transpose Matrix..cpp
+++ 2023/DECEMBER/10th Dec__Transpose Matrix..cpp
@@ -2,6 +2,37 @@ PROBLEM:https://leetcode.com/problems/transpose-matrix/description/?envType=dail
 VIDEO (MIK BHAIYA):
 
 /*
-  TC:
-  SC:
+  TC: O(m*n)
+  SC: O(1)
 */
+
+class Solution {
+public:
+    
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>>result(n,vector<int>(m));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               result[j][i]=matrix[i][j];
+            }
+        }
+        return result;
+    }
+};
+
+
+/*
+
+  FOLLOW UP QUESTION .........WHAT IF THE MATRIX WAS A SQUARE MATRIX...........?
+
+  */
+
+for(int i=0;i<n;i++){
+  for(int j=i+1;j<n;j++){
+    swap(mat[i][j],mat[j],mat[i]);
+  }
+}
+
+
