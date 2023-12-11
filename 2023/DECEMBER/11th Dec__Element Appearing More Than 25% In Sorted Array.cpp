751241927101a2011685a43cd66be53c5193adcc
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-11 08:37:53 -0800

    Update 11th Dec__Element Appearing More Than 25% In Sorted Array.cpp

--- 2023/DECEMBER/11th Dec__Element Appearing More Than 25% In Sorted Array.cpp
+++ 2023/DECEMBER/11th Dec__Element Appearing More Than 25% In Sorted Array.cpp
@@ -41,3 +41,63 @@ public:
 
     }
 };
+
+/*
+  METHOD 3
+  TC: o(3*logn)
+  SC:
+  */
+
+class Solution {
+public:
+      int findRightIndex(int cand,int l,int r,vector<int>&arr){
+        int result_idx=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==cand){
+                result_idx=mid;
+                l=mid+1;
+            }
+            else if(arr[mid]<cand){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return result_idx;
+    }
+    int findLeftIndex(int cand,int l,int r,vector<int>&arr){
+        int result_idx=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==cand){
+                result_idx=mid;
+                r=mid-1;
+            }
+            else if(arr[mid]<cand){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return result_idx;
+    }
+    int findSpecialInteger(vector<int>& arr) {
+        int n=arr.size();
+        int freq=n/4;
+       
+        vector<int>candidates{arr[n/4],arr[n/2],arr[3*n/4]};
+        for(int &cand:candidates)
+        {
+            int left_idx=findLeftIndex(cand,0,n-1,arr);
+            int right_idx=findRightIndex(cand,0,n-1,arr);
+            if(right_idx-left_idx+1>freq)return cand;
+
+        }
+
+        return -1;
+
+    }
+};
