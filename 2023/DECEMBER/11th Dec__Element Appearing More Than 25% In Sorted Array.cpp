1205e3c40c965f72dd0a0418ee7fe3fc796e462c
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-11 07:40:56 -0800

    Update 11th Dec__Element Appearing More Than 25% In Sorted Array.cpp

--- 2023/DECEMBER/11th Dec__Element Appearing More Than 25% In Sorted Array.cpp
+++ 2023/DECEMBER/11th Dec__Element Appearing More Than 25% In Sorted Array.cpp
@@ -21,3 +21,23 @@ public:
 
     }
 };
+
+/*
+  METHOD 2:
+  TC:O(N)
+  SC:O(1)
+  */
+
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n=arr.size();
+        int freq=n/4;
+       
+        for(int i=0;i<n-freq;i++){
+            if(arr[i]==arr[i+freq])return arr[i];
+        }
+        return -1;
+
+    }
+};
