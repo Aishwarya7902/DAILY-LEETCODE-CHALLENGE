55e4c0daa5c042a0404011f5f476100bac50f6f5
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-12 08:57:08 -0800

    Update 12th Dec__Maximum Product of Two Elements in an Array.cpp

--- 2023/DECEMBER/12th Dec__Maximum Product of Two Elements in an Array.cpp
+++ 2023/DECEMBER/12th Dec__Maximum Product of Two Elements in an Array.cpp
@@ -19,3 +19,27 @@ public:
         return result;
     }
 };
+
+/*
+method 2
+  tc:o(n)
+  sc:o(1)
+*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int largest=0;
+        int second_largest=0;
+
+        for(int &num:nums){
+            if(num>largest){
+                second_largest=largest;
+                largest=num;
+            }
+            else{
+                second_largest=max(second_largest,num);
+            }
+        }
+        return (largest-1)*(second_largest-1);
+    }
+};
