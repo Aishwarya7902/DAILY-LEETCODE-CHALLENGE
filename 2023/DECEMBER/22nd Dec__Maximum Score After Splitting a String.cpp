aa1f66b0174c052497d629783d98859bd04560c8
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-22 01:41:32 -0800

    Update 22nd Dec__Maximum Score After Splitting a String.cpp

--- 2023/DECEMBER/22nd Dec__Maximum Score After Splitting a String.cpp
+++ 2023/DECEMBER/22nd Dec__Maximum Score After Splitting a String.cpp
@@ -70,3 +70,36 @@ public:
         return result;
     }
 };
+
+//Approach-3 (1 Pass - Use equation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int maxScore(string s) {
+        int n = s.length();
+        int result = INT_MIN;
+
+        
+        int zeros = 0;
+        int ones  = 0;
+        
+        for(int i = 0; i < n-1; i++) {
+            if(s[i] == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+            
+            result = max(result, zeros - ones);
+        }
+        
+        if(s[n-1] == '1') {
+            ones++;
+        }
+        
+        return result+ones;
+    }
+};
+
+
