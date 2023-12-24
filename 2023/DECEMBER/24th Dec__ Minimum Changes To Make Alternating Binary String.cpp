d15a18afc54cbb1e9c7f5f946817e43b0e3fc8a3
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-24 02:38:12 -0800

    Update 24th Dec__ Minimum Changes To Make Alternating Binary String.cpp

--- 2023/DECEMBER/24th Dec__ Minimum Changes To Make Alternating Binary String.cpp
+++ 2023/DECEMBER/24th Dec__ Minimum Changes To Make Alternating Binary String.cpp
@@ -3,8 +3,8 @@ VIDEO (MIK BHAIYA) :https://www.youtube.com/watch?v=YB4oUR2Skfk
 
 /*
   METHOD 1
-  TC:
-  SC:
+  TC: o(n)
+  SC: o(1)
 */
 
 class Solution {
@@ -27,6 +27,33 @@ public:
 
             }
         }
+        return min(start_with_0,start_with_1);
+    }
+};
+
+
+/*
+little optimization
+*/
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int n=s.size();
+        int start_with_0=0; //01010101 ........even index should have 0 and odd index should have 1
+        
+
+        for(int i=0;i<n;i++){
+            if(i%2==0){ //even index
+             if(s[i]=='1')
+              start_with_0++;
+
+            }
+            else{ //odd_index
+              if(s[i]=='0')start_with_0++;
+            }
+        }
+        int start_with_1=n-start_with_0;
         return min(start_with_0,start_with_1);
     }
 };
