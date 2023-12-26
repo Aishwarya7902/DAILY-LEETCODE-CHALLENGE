81ca3d20dda777ccb7079dda358287e9dc479224
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-26 00:58:29 -0800

    Update 26th Dec__Number of Dice Rolls With Target Sum.cpp

--- 2023/DECEMBER/26th Dec__Number of Dice Rolls With Target Sum.cpp
+++ 2023/DECEMBER/26th Dec__Number of Dice Rolls With Target Sum.cpp
@@ -28,4 +28,31 @@ public:
 
     }
 };
+
+
+/*
+MEMOIZATION
+TC:
+SC:
+*/
+class Solution {
+public:
+    int MOD=1e9+7;
+    int dp[31][1001];
+    int solve(int n,int k,int target){
+        if(target<0)return 0;
+        if(n==0) return target==0;
+        if(dp[n][target]!=-1)return dp[n][target];
+        int ways=0;
+        for(int face=1;face<=k;face++){
+            ways=(ways+solve(n-1,k,target-face))%MOD;
+        }
+        return  dp[n][target]=ways;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+       memset(dp,-1,sizeof(dp));
+       return solve(n,k,target); 
+
+    }
+};
   
