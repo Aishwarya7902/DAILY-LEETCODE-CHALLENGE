7d591ddba32514d4e2d437eb4dca94b46a4bfac9
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-30 05:46:08 -0800

    Update 30th Dec__ Redistribute Characters to Make All Strings Equal.cpp

--- 2023/DECEMBER/30th Dec__ Redistribute Characters to Make All Strings Equal.cpp
+++ 2023/DECEMBER/30th Dec__ Redistribute Characters to Make All Strings Equal.cpp
@@ -4,8 +4,7 @@ VIDEO:
 /*
   approach 1
   TC: o(m*n) // m is size of words and n is size of longest string
-  SC: o(26) as  words[i] consists of lowercase English letters.
-
+  SC: o(n) 
   */
 
 class Solution {
@@ -21,6 +20,32 @@ public:
 
        for(auto it:mp){
          int freq=it.second;
+         if(freq%n!=0)return false;
+       }
+
+       return true;
+    }
+};
+
+/*
+OPTIMISATION OF SPACE USING ARRAY
+TC:O(M*N)
+SC:O(26)
+*/
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+       int n=words.size();
+       int arr[26]={0};
+       for(auto &str:words){
+           for(char &ch:str){
+               arr[ch-'a']++;
+           }
+       } 
+
+       for(auto &freq:arr){
+         
          if(freq%n!=0)return false;
        }
 
