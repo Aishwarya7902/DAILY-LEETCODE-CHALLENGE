e9f510aeae3437bb2a231555b9817302b4cdca3f
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-31 02:36:22 -0800

    Update 31__Dec__ Largest Substring Between Two Equal Characters.cpp

--- 2023/DECEMBER/31__Dec__ Largest Substring Between Two Equal Characters.cpp
+++ 2023/DECEMBER/31__Dec__ Largest Substring Between Two Equal Characters.cpp
@@ -1,5 +1,5 @@
 PROBLEM :https://leetcode.com/problems/largest-substring-between-two-equal-characters/description/?envType=daily-question&envId=2023-12-31
-VIDEO(MIK BHAIYA):
+VIDEO(MIK BHAIYA):https://www.youtube.com/watch?v=kQLhWLVlzYo
 
   /*
   DID ON MY OWN 🥳
@@ -48,3 +48,56 @@ void FindLastIndex(string &s,char target, vector<int>&LastIndex){
         
     }
 };
+
+
+/*
+  METHOD 2 :USING MAP
+  TC:O(N)
+  SC:O(N)
+  */
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char,int>mp;
+        int result=-1;
+        for(int i=0;i<s.size();i++){
+            if(mp.find(s[i])==mp.end()){
+                mp[s[i]]=i;
+            }
+            else{
+                result=max(result,i-mp[s[i]]-1);
+            }
+
+        }
+        return result;
+    }
+};
+
+
+/*
+  METHOD 3 :USING VECTOR OF FIXED SIZE
+  TC:O(N)
+  SC:O(1)
+  */
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        vector<int>arr(26,-1);
+        int result=-1;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(arr[ch-'a']==-1){
+                arr[ch-'a']=i;
+            }
+            else{
+                result=max(result,i-arr[ch-'a']-1);
+            }
+
+        }
+        return result;
+    }
+};
+  
+  
